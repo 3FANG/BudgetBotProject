a3a908eeb8fdc7970634553262642f679db52cf1
Deni <d.hasimikov@mail.ru>
2023-05-06 14:59:20 +0300

    Add SQLAlchemy Base and Models

---
+++ bot/database/__init__.py
@@ -0,0 +1,10 @@
+from .base import Base
+from .models import Budget, Category, Expense
+
+
+__all__ = [
+    Base,
+    Budget,
+    Category,
+    Expense
+]
---
+++ bot/database/models.py
@@ -0,0 +1,42 @@
+from typing import List, Optional
+import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, ForeignKey
+
+from bot.database.base import Base
+
+
+class Budget(Base):
+    __tablename__ = "User"
+
+    codename: Mapped[str] = mapped_column(String(255), primary_key=True)
+    daily_limit: Mapped[int]
+
+    def __repr__(self) -> str:
+        return f"Budget(codename={self.codename!r}, daily_limit={self.daily_limit!r})"
+
+
+class Category(Base):
+    __tablename__ = "Category"
+
+    codename: Mapped[str] = mapped_column(String(255), primary_key=True)
+    name: Mapped[str] = mapped_column(String(255))
+    is_base_expanse: Mapped[bool]
+    aliases: Mapped[str]
+
+    def __repr__(self) -> str:
+        return f"Category(codename={self.codename!r}, name={self.name!r}, is_base_expense={self.is_base_expanse!r}, aliases={self.aliases!r})"
+
+
+class Expense(Base):
+    __tablename__ = "Expense"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    amount: Mapped[int]
+    created: Mapped[datetime.datetime]
+    category_codename = mapped_column(ForeignKey("Category.codename"))
+    raw_text: Mapped[str]
+
+    def __repr__(self) -> str:
+        return f"Expense(id={self.id!r}, amount={self.amount!r}, created={self.created!r}, category_codename={self.category_codename!r}, raw_text={self.raw_text!r})"

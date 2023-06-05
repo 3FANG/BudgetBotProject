234049f011eb4b561667b18548661585a766d4a9
Deni <d.hasimikov@mail.ru>
2023-06-05 10:43:15 +0300

    Set db_url in alembic/env.py

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from bot.database import Base
+from bot.config.config import load_config
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,6 +22,12 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
+
+bot_config = load_config()
+config.set_main_option(
+    'sqlalchemy.url',
+    bot_config.db.url_object
+)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:

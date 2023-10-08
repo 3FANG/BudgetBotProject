f42b98acef7cfcabbe42cb58516a750fd29d9d91
3FANG <d.hasimikov@mail.ru>
2023-10-08 15:57:21 +0300

    a bit regularize RU_LEXICON

--- bot/lexicon/lexicon.py
+++ bot/lexicon/lexicon.py
@@ -1,13 +1,16 @@
 RU_LEXICON = {
+    #messages
     'start_message': "Стартовое сообщение",
-    'expenses_button': "Расходы",
-    'statistics_button': "Статистика",
+    'statistics_message': "Сообщение вкладки статистики",
+    'expenses_message': "Сообщение вкладки расходов",
+
+    #buttons
     'add_category_button': "Добавить категорию",
     'add_alias_button': "Добавить обращение",
-    'expenses_message': "Сообщение вкладки расходов",
+    'expenses_button': "Расходы",
+    'statistics_button': "Статистика",
     'main_menu_button': "Главное меню",
     'add_expense_button': "Добавить расход",
-    'statistics_message': "Сообщение вкладки статистики",
     'today_statistics_button': "Сегодня",
     'current_month_statistics_botton': "Текущий Месяц",
     'select_period_statistics_button': "Выбрать период"

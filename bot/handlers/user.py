c72f40aff7dc8eeca4a23b91828fa3079db91383
3FANG <d.hasimikov@mail.ru>
2023-10-08 15:53:00 +0300

    Add expenses and statistics menu handlers

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -1,17 +1,18 @@
 import datetime
 
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.dialects.postgresql import insert
 
 from bot.lexicon import RU_LEXICON
 from bot.database import Users
-# from sqlalchemy.orm import Session
-
-
-# session: Session
+from bot.keyboards import (
+    start_keyboard,
+    expenses_keyboard,
+    statistics_keyboard
+)
 
 
 router = Router()
@@ -19,7 +20,6 @@ router = Router()
 
 @router.message(CommandStart())
 async def start_command(message: Message, session: AsyncSession):
-    """Отправляет приветственное сообщение"""
     user = {
         'id': message.from_user.id,
         'username': message.from_user.username,
@@ -30,8 +30,19 @@ async def start_command(message: Message, session: AsyncSession):
     await session.execute(insert(Users).values(user).on_conflict_do_nothing())
     await session.commit()
 
-    await message.answer(text=RU_LEXICON['start_message'])
-
-    '''Оптимизация!!!!'''
+    await message.answer(text=RU_LEXICON['start_message'], reply_markup=start_keyboard())
 
 
+@router.callback_query(F.data == 'main_menu')
+async def main_menu_callback(callback: CallbackQuery):
+    await callback.message.edit_text(text=RU_LEXICON['start_message'], reply_markup=start_keyboard())
+
+
+@router.callback_query(F.data == 'expenses')
+async def expenses_callback(callback: CallbackQuery):
+    await callback.message.edit_text(text=RU_LEXICON['expenses_message'], reply_markup=expenses_keyboard())
+
+
+@router.callback_query(F.data == 'statistics')
+async def statistics_callback(callback: CallbackQuery):
+    await callback.message.edit_text(text=RU_LEXICON['statistics_message'], reply_markup=statistics_keyboard())
